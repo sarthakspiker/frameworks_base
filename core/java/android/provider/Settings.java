6d8d3a9ea5308bb1c3da144bda9522b8e5a1edc6
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-02-01 06:41:03 +0000

    QS: Option to show four tiles per row (1/2)
    This also reduces the size of the ripple when using 4 tiles per row
    
    Squash of the following:
    *QS: reduce TILE_ASPECT when using 4 tiles per row
    *QS: don't resize dual tiles when using 4 per row
    *QS: move all options into a SettingsObserver in QSPanel
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6612,6 +6612,12 @@ public final class Settings {
         public static final String QS_SHOW_BRIGHTNESS_SLIDER = "qs_show_brightness_slider";
 
         /**
+         * Whether to show four tiles per row.
+         * @hide
+         */
+        public static final String QS_USE_FOUR_COLUMNS = "qs_use_four_columns";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
