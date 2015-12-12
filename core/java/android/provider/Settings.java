db4106142c53b1c084d8544914e29fb97f2dfa0e
Jorge Martín <angel.arasthel@gmail.com>
2015-12-11 20:38:26 -0500

    Add customization for Quick Settings [1/2] Allow Quick Settings tiles to be arranged any way you want, delete them or add some.
    I removed the "dual" status that was hardcoded into Wifi and BT tiles and added it
    as a variable which is true when the tile is on the first row.
    
    Also, the first row is now hardcoded to have only 2 items to follow the same design as before.
    
    PureNexus edits: Adapt for Marshmallow, Move Settings to Secure

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6618,6 +6618,19 @@ public final class Settings {
         public static final String QS_USE_FOUR_COLUMNS = "qs_use_four_columns";
 
         /**
+         * List of QS tile names
+         * @hide
+         */
+        public static final String QS_TILES = "sysui_qs_tiles";
+
+        /**
+         * Use "main" tiles on the first row of the quick settings panel
+         * 0 = no, 1 = yes
+         * @hide
+         */
+        public static final String QS_USE_MAIN_TILES = "sysui_qs_main_tiles";
+
+        /**
          * This are the settings to be backed up.
          *
          * NOTE: Settings are backed up and restored in the order they appear
